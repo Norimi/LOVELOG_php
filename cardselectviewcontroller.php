b569def9cb07d905ba1fb23212aae1ac73fabd0b
noriming <noriming@gmail.com>
2014-07-24 14:35:38 +0900

    cardtitleviewcontroller.php

--- cardselectviewcontroller.php
+++ cardselectviewcontroller.php
@@ -1,12 +1,12 @@
 <?php
+
 header("Content-Type:text/xml; charset=UTF-8");
 session_start();
 
     
-    mysql_connect('mysql712.xserver.jp', 'noriming_lovelog', 'withlovelogbear') or die(mysql_error());
-    mysql_select_db('noriming_lovelog');
-    mysql_query('SET NAMES UTF8');
-
+mysql_connect('mysql712.xserver.jp', 'noriming_lovelog', 'withlovelogbear') or die(mysql_error());
+mysql_select_db('noriming_lovelog');
+mysql_query('SET NAMES UTF8');
 
     
 $sql = sprintf('SELECT * FROM ld2cards WHERE userid=%d OR userid=%d ORDER BY cardid DESC',
@@ -15,20 +15,13 @@ mysql_real_escape_string($_SESSION['pid']),
                );
 $recordSet = mysql_query($sql) or die(mysql_error());
 
-
 ?>
 
 <body>
 
 <?php
-
 while($carditems = mysql_fetch_assoc($recordSet)):
-
-
 ?>
-
-
-
 
 <content>
 <title><?php echo htmlspecialchars($carditems['title'], ENT_QUOTES, 'UTF-8'); ?></title>
---
+++ cardtitleviewcontroller.php
@@ -0,0 +1,33 @@
+<?php
+
+mysql_connect('mysql712.xserver.jp', 'noriming_lovelog', 'withlovelogbear') or die(mysql_error());
+mysql_select_db('noriming_lovelog');
+mysql_query('SET NAMES UTF8');
+
+
+if(isset($_POST)){
+
+  $sql = sprintf('INSERT INTO ld2cards SET message="%s", title="%s", cardname="%s", userid=%d, date="%s"',
+  mysql_real_escape_string($_REQUEST['message']),
+  mysql_real_escape_string($_REQUEST['title']),
+  mysql_real_escape_string($_REQUEST['cardname']),
+  mysql_real_escape_string($_REQUEST['userid']),
+  date('Y-m-d H:i:s')
+);
+
+mysql_query($sql)or die(mysql_error());
+
+
+//chatの方への出力        
+$chat = 'あたらしいカードを贈りました。';
+$sql = sprintf('INSERT INTO ld2chat SET userid=%d, chat="%s", date="%s"',
+                       mysql_real_escape_string($_POST['userid']),
+                       mysql_real_escape_string($chat),
+                       date('Y-m-d H:i:s')
+                       );
+mysql_query($sql) or die(mysql_error());  
+
+
+}
+
+?>
