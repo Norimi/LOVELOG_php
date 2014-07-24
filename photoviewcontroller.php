65b922b561f732e1bbbf1b9392b2dee9110dbfa8
noriming <noriming@gmail.com>
2014-07-24 16:07:58 +0900

    photoviewcontroller.php

---
+++ photoviewcontroller.php
@@ -0,0 +1,39 @@
+<?php
+
+
+  mysql_connect('mysql712.xserver.jp', 'noriming_lovelog', 'withlovelogbear') or die(mysql_error());
+  mysql_select_db('noriming_lovelog');
+  mysql_query('SET NAMES UTF8');
+
+
+  if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
+    
+    $userid = $_POST['userid'];
+    $extension = $_POST['extension'];
+    $image = date('YmdHis') .$userid .$extension;
+    //エラーをチェックしてファイル名をつくる。userid?を入れる。
+     move_uploaded_file($_FILES["upfile"]["tmp_name"], "./photo_uploaded/" .$image);  
+
+    //DLするときのためファイル情報をServerに入れる
+    $sql = sprintf('INSERT INTO ld2photos SET userid=%d, title="%s",filename="%s", created="%s"',
+    mysql_real_escape_string($_POST['userid']),
+    mysql_real_escape_string($_POST['title']),
+    mysql_real_escape_string($image),
+    date('Y-m-d H:i:s')
+    );
+    mysql_query($sql) or die(mysql_error());
+    
+    //chatにメッセージを挿入
+    $message='あたらしい写真を投稿しました。';
+    $phototitle = $_POST['title'];
+    $chat = sprintf("%s 「%s」", $message, $phototitle);
+    $sql = sprintf('INSERT INTO ld2chat SET userid=%d, chat="%s",date="%s"',
+                   mysql_real_escape_string($_POST['userid']),
+                   mysql_real_escape_string($chat),
+                   date('Y-m-d H:i:s')
+                   );
+    mysql_query($sql) or die(mysql_error());
+
+    }
+
+?>
